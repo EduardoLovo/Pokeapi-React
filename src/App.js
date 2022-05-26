ff785aa981dd8ffd9dc06b721ed68c5efb9099ac
EduardoLovo <eduardo.llovo@gmail.com>
2022-05-26 11:13:09 -0300

    update api, create pokemon card

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
+import { useEffect, useState } from 'react';
+import { getPokemonData, getPokemons } from './Api/Api';
 import './App.css';
 import { Navbar } from './components/Navbar/Navbar';
 import { Pokedex } from './components/Pokedex/Pokedex';
 import { Searchbar } from './components/Seachbar/Searchbar';
 
 function App() {
+  const [loading, setLoading] = useState(false)
+  const [pokemons, setPokemons] = useState([])
+
+  const fetchPokemons = async () => {
+    try {
+      setLoading(true)
+      const data = await getPokemons();
+      const promises = data.results.map(async (pokemon) => {
+        return await getPokemonData(pokemon.url)
+      });
+      const results = await Promise.all(promises);
+      setPokemons(results)
+      setLoading(false)
+    } catch (error) {
+      console.log('error:', error);
+    }
+  }
+
+  useEffect(() => {
+    fetchPokemons()
+    console.log('carregou');
+  }, [])
 
   return (
     <div className="App">
       <Navbar />
       <Searchbar />
-      <Pokedex />
+      <Pokedex pokemons={pokemons} loading={loading} />
     </div>
   );
 }
